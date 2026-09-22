7e3e9222bb43b4fba59d70996d0492e0dffe83a2
afra55 <afra55@foxmail.com>
2026-09-22 21:15:09 +0800

    chore(黑盒): 移除结果卡片上的版本标记（排查结束，不再需要）

--- tools/lib/tools-build.js
+++ tools/lib/tools-build.js
@@ -1,6 +1,6 @@
 (() => {
   "use strict";
-  const BUILD = "2026.09.22-210642";
+  const BUILD = "2026.09.22-211509";
   window.TOOLS_BUILD = BUILD;
   window.TOOLS_VERSION = BUILD;
 
--- tools/sw.js
+++ tools/sw.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-restricted-globals */
 "use strict";
 
-const SHELL_CACHE = "devtools-shell-20260922-210642";
+const SHELL_CACHE = "devtools-shell-20260922-211509";
 const PRECACHE = [
   "./",
   "./index.html",
